4e3eafac74717c95fa24af9b399087d56223b4df
pedroblasco <pedroblascoibarra@gmail.com>
2013-01-07 09:54:27 +0100

    Añadiendo HttpServer con función de múltiples peticiones.

--- HttpServer/src/HttpServer.java
+++ HttpServer/src/HttpServer.java
@@ -19,53 +19,54 @@ public class HttpServer {
 		final String response200 = "HTTP/1.0 200 OK";
 		final String response404 = "HTTP/1.0 404 Not Found";
 		ServerSocket serverSocket = new ServerSocket(port);
-		
-		Socket socket = serverSocket.accept();
-		Scanner scanner = new Scanner(socket.getInputStream());
-		
-		String fileName="index.html";
-		
 		while(true){
-			String line = scanner.nextLine();
-			System.out.println(line);
-			if (line.equals(""))
-				break;
+			Socket socket = serverSocket.accept();
+			Scanner scanner = new Scanner(socket.getInputStream());
+			
+			String fileName="index.html";
+			
+			while(true){
+				String line = scanner.nextLine();
+				System.out.println(line);
+				if (line.equals(""))
+					break;
+			}
+			
+			File file = new File (fileName);
+			
+			String responseFileName = file.exists() ? fileName : fileNameError404;
+			String response = file.exists() ? response200 : response404;
+			
+			String header = response + newLine + newLine;
+			
+			byte[] headerBuffer = header.getBytes();
+			
+			OutputStream outputStream = socket.getOutputStream();
+			outputStream.write(headerBuffer);
+			
+			final int bufferSize = 2048;
+			byte[] buffer = new byte[bufferSize];
+			
+			FileInputStream fileInputStream = new FileInputStream(responseFileName);
+			int count;
+			
+			while ((count = fileInputStream.read(buffer))!=-1){
+				//La línea siguiente es la que utilizamos para provocar un retardo en las peticiones.
+				Thread.sleep(5000);
+				outputStream.write(buffer, 0, count);
+			}
+			//System.setProperty("line.separator", "\r\n");
+			
+			/*PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+			
+			printWriter.println("HTTP/1.0 404 Not Found");
+			printWriter.println(newLine);
+			
+			printWriter.flush();*/
+			
+			fileInputStream.close();
+			socket.close();
 		}
-		
-		File file = new File (fileName);
-		
-		String responseFileName = file.exists() ? fileName : fileNameError404;
-		String response = file.exists() ? response200 : response404;
-		
-		String header = response + newLine + newLine;
-		
-		byte[] headerBuffer = header.getBytes();
-		
-		OutputStream outputStream = socket.getOutputStream();
-		outputStream.write(headerBuffer);
-		
-		final int bufferSize = 2048;
-		byte[] buffer = new byte[bufferSize];
-		
-		FileInputStream fileInputStream = new FileInputStream(responseFileName);
-		int count;
-		
-		while ((count = fileInputStream.read(buffer))!=-1)
-			//La línea siguiente es la que utilizamos para provocar un retardo en las peticiones.
-			Thread.sleep(5000);
-			outputStream.write(buffer, 0, count);
-		
-		//System.setProperty("line.separator", "\r\n");
-		
-		/*PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-		
-		printWriter.println("HTTP/1.0 404 Not Found");
-		printWriter.println(newLine);
-		
-		printWriter.flush();*/
-		
-		fileInputStream.close();
-		socket.close();
-		serverSocket.close();
+		//serverSocket.close();
 	}
 }
